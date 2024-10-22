0c4a55054ee2ebe9af8c9ea1b0107519d7d93a12
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-10-22 15:49:00 +0500

    update

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -11,24 +11,24 @@ const Products = () => {
   const images5 = [img2, img3, img1];
   const images6 = [img3, img1, img2];
 
-  const allImages = [images1, images2, images3, images4, images5, images6]; 
+  const allImages = [images1, images2, images3, images4, images5, images6];
 
   const [currentImages, setCurrentImages] = useState(Array(6).fill(0));
 
   const handleMouseMove = (e, cardIndex) => {
     const { width } = e.target.getBoundingClientRect();
     const xPos = e.nativeEvent.offsetX;
-    const newImages = [...currentImages];  
+    const newImages = [...currentImages];
 
     if (xPos < width / 3) {
-      newImages[cardIndex] = 0;  
+      newImages[cardIndex] = 0;
     } else if (xPos < (2 * width) / 3) {
-      newImages[cardIndex] = 1; 
+      newImages[cardIndex] = 1;
     } else {
-      newImages[cardIndex] = 2;  
+      newImages[cardIndex] = 2;
     }
 
-    setCurrentImages(newImages); 
+    setCurrentImages(newImages);
   };
 
   return (
